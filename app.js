82f5b554a61b592d971096c6f7dd51017d6eb938
skan <dydrnrdks@gmail.com>
2019-08-14 18:20:26 +0900

    app.js delete

--- app.js
+++
@@ -1,6 +0,0 @@
-console.log("hello nodejsyrdy");
-var steem = require("steem");
-steem.api.setOptions({url: 'https://api.steemit.com'});
-steem.api.getAccounts(['ned','dan'], function(err, response){
-  console.log(err,response);
-});
